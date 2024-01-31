cddb84a07a86c3d2af5c22cbc075bdcea0c9dba8
MohammadSaquib786 <mohdsaquib7068@gmail.com>
2024-01-31 18:29:50 +0530

    Changes

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const reviews = [
+    {
+        id:1,
+        name:"Mohammad Saquib" ,
+        job: "Software Engineer",
+        image:"https://media.licdn.com/dms/image/D5603AQH7mhhi_mywFQ/profile-displayphoto-shrink_800_800/0/1705925356377?e=1712188800&v=beta&t=CBh10QQrd9yE96rw5G8UH_g8jUVvcbNnEisgZ24esTk",
+        text:"Software engineering is a multifaceted discipline that encompasses the systematic design, development, testing, and maintenance of software systems. It involves applying engineering principles to create reliable, efficient, and scalable software solutions to address diverse user needs and requirements. Software engineers utilize various methodologies, such as Agile or Waterfall, to manage the software development lifecycle, from conceptualization to deployment. ",
+
+    },
+    {
+        id:2,
+        name:"Mohd Hadi" ,
+        job: "Graphics Designer",
+        image:"https://media.licdn.com/dms/image/D4D03AQE3751FQ946vg/profile-displayphoto-shrink_800_800/0/1696876123337?e=1712188800&v=beta&t=NhKO41mzsuWSNpy65UMzJKd3AG8auyItPqWQUDP8lW8",
+        text:"A graphics designer is a creative professional who specializes in visual communication, utilizing various mediums such as digital or print to convey messages, ideas, and concepts effectively. Their role involves conceptualizing, designing, and producing graphics that enhance the aesthetic appeal and usability of products, services, or information. Graphics designers possess a keen eye for detail, color, composition, and typography, enabling them to create visually compelling designs that resonate with target audiences. They often collaborate with clients. ",
+
+    },
+    {
+        id:3,
+        name:"Sahil Afridi" ,
+        job: "Marketing Manager",
+        image:"https://media.licdn.com/dms/image/D4D03AQE0Q5pXgO8JcA/profile-displayphoto-shrink_800_800/0/1693105255819?e=1712188800&v=beta&t=W1_ZqKllRifMCQPcXEC7RqJC0Mw5uAYz8SMpAY2y24c",
+        text:"A marketing manager is a strategic leader responsible for overseeing all aspects of a company's marketing activities with the goal of promoting its products or services, driving sales, and enhancing brand awareness. Their role involves developing and implementing comprehensive marketing strategies that align with the company's objectives and target audience. Marketing managers conduct market research to identify consumer trends, preferences, and competitive landscapes, informing decision-making processes and campaign development.",
+
+    },
+    {
+        id:4,
+        name:"Akhilesh" ,
+        job: "Content Strategist",
+        image:"https://media.licdn.com/dms/image/D4D03AQHAHKjzuxqGBA/profile-displayphoto-shrink_800_800/0/1686132186517?e=1712188800&v=beta&t=qm-_J7uOwKUA3f390jYfAHn8zROkHQqWZbfDunhZdO0",
+        text:"A content strategist is a key player in the realm of digital marketing and content creation, responsible for developing and implementing a cohesive plan to guide the creation, distribution, and management of content across various platforms. Their role involves understanding the target audience, market trends, and business goals to craft strategic content that resonates with users and drives engagement. Content strategists conduct thorough research to identify topics, keywords.",
+
+    },
+    {
+        id:5,
+        name:"Sumit" ,
+        job: "Data Analyst",
+        image:"https://media.licdn.com/dms/image/D4D03AQFY8RXiRPkm2g/profile-displayphoto-shrink_800_800/0/1700636875459?e=1712188800&v=beta&t=IEdqOa_WdCNdpSW9xxVFUnIuRU4gDTMAoijtS4EWBaM",
+        text:"A data analyst is a skilled professional who specializes in collecting, processing, and analyzing data to derive actionable insights and inform decision-making processes within an organization. Their role involves gathering data from various sources, such as databases, spreadsheets, and APIs, and cleaning and organizing it to ensure accuracy and consistency. Data analysts utilize statistical techniques, data mining.",
+
+    },
+    
+];
+
+export default reviews;
